33b4ea7086b9840667a7a6df4791b3671aa4bdd2
Zimp4er <129596653+Zimp4er@users.noreply.github.com>
2023-04-08 15:27:05 +0300

    Add files via upload
    Add second player

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -1,7 +1,26 @@
+from random import *
+from pygame import *
+FPS = 60
+lost = 0
+score = 0
+max_lost = 3
+speed = 10
+win_width = 700
+win_height = 500
+life = 5
+clock = time.Clock()
+mixer.init()
+mixer.music.load('space.ogg')
+fire_sound = mixer.Sound("fire.ogg")
+mixer.music.play()
+font.init()
+font1 = font.Font(None, 36)
+lose = font1.render("Ты проиграл", 20 , (255, 255, 255))
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65,65))
+        self.image = transform.scale(image.load(player_image), (65,100))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -13,10 +32,41 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= speed
-        if keys_pressed[K_RIGHT] and self.rect.x < 650:
-            self.rect.x += speed
-    def fire(self):
-        bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top,   15)
-        bullets.add(bullet)
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= speed
+        if keys_pressed[K_DOWN] and self.rect.y < 435:
+            self.rect.y += speed
+
+class Player2(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= speed
+        if keys_pressed[K_s] and self.rect.y < 435:
+            self.rect.y += speed
+
+window = display.set_mode((700,500))
+display.set_caption('Ping_Pong')
+background = transform.scale(image.load('12834.jpg'), (700,500))
+
+player = Player2('Stick.png', -15, 250, 5)
+player2 = Player('Stick.png', 650, 250, 5)
+game = True
+finish = False
+while game:
+    window.blit(background,(0,0))
+    for e in event.get():
+
+        if e.type == QUIT:
+            game = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                player.fire()
+                fire_sound.play()
+    if not finish:
+        player.reset()
+        player2.reset()
+        player.update()
+        player2.update()
+        clock.tick(FPS)
+        display.update()
